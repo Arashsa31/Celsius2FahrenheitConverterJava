37afebe7b97262ae7412b40e580fc0295628652a
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-04-02 12:39:21 -0400

    Add files via upload

---
+++ CtoFconverter.java
@@ -0,0 +1,44 @@
+/**
+ * @author Arash
+ * This program takes a Celsius temperature input and converts it to its equivalent in Fahrenheit temperature
+ * The conversion formula used is: Fahrenheit = 9/5 * Celsius +32 
+ */
+
+
+/**
+ * import packets
+ */
+
+//import java.util.Scanner; 
+import java.util.*;
+
+public class CtoFconverter
+{
+	public static void main(String[] args) 
+	{
+		// declare variables
+		double fTemp, cTemp;
+		
+		// creates scanner object
+		Scanner keyboard = new Scanner(System.in);
+		
+		// asks for the user input
+		System.out.print("Please enter the current temperature in Celsius: ");
+		
+		//takes the user input and places it into the cTemp variable 
+		cTemp = keyboard.nextDouble(); 
+	    
+	    // repeats what the user entered 	    
+	    System.out.print("You entered the current temperature as " + cTemp + " degrees in Celsius.\n");
+	    
+		//use the conversion formula to convert Fahrenheit temperature to Celsius temperature
+		fTemp = 9.0/5.0*cTemp+32.0;
+	    
+	    // prints the converted temperature fTemp to the user
+	    System.out.print("Your converted temperature of " + cTemp + " Celsius ");
+	    System.out.printf("in Fahrenheit is: %.3f degrees.\n", fTemp ); //rounds the decimal up to 3 points
+
+	    //closes scanner object
+	    keyboard.close();
+	}
+}
